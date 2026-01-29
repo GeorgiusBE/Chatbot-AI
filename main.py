628d64d666eaf4939d7e457b63fbd0e9fd182867
Georgius <georgiusbenedikt09@gmail.com>
2026-01-29 22:10:37 +0000

    Add error if API keys not provided

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 import os
 
-# load variables in .env
+# load variables in .env locally (if present)
 from dotenv import load_dotenv
 load_dotenv()
+
+# ensure that OpenAI and LangChain API key is provided, otherwise the app stops
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") or st.secrets.get("OPENAI_API_KEY", None)
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY") or st.secrets.get("LANGCHAIN_API_KEY", None)
+
+if not OPENAI_API_KEY:
+    st.error("Missing OPENAI_API_KEY")
+    st.stop()
+
+if not LANGCHAIN_API_KEY:
+    st.error("Missing LANGCHAIN_API_KEY")
+    st.stop()
+
+
 
 # define prompt template
 prompt = ChatPromptTemplate(
