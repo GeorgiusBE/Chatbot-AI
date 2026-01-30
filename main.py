cc18f92ac45df6e6fca20373bdf029a85a297709
Georgius <georgiusbenedikt09@gmail.com>
2026-01-30 22:06:23 +0000

    Add RAG for pdfs

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import streamlit as st
-import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-import os
+
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+
+import os, tempfile, shutil
+from pathlib import Path
 
 # load variables in .env locally (if present)
 from dotenv import load_dotenv
@@ -22,6 +29,115 @@ if not LANGCHAIN_API_KEY:
     st.stop()
 
 
+
+# --- Back-end model ---
+## Save Streamlit UploadedFile objects to disk and return saved paths
+def save_uploads_to_dir(uploaded_files, target_dir:Path) -> list[Path]:
+    '''Save Streamlit UplaodedFile objects to disk and return saved paths'''
+    target_dir.mkdir(parents=True, exist_ok=True) # create folder to store the uploaded files
+    saved = []
+    for uf in uploaded_files:
+        out = target_dir / uf.name # define the file path
+        out.write_bytes(uf.getbuffer())
+            # "uf.getbuffer()" gets the raw bytes of the uploaded file.
+            # "write_bytes(...)" writes those bytes to a real file on disk at "out".
+        saved.append(out) # store file path of saved files
+    return saved
+
+## create RAG retriever
+def create_rag_retriever(pdf_dir: Path):
+    # data ingestion: On all pdf files in a folder
+    loader = DirectoryLoader(
+        path=str(pdf_dir), # open this folder path
+        glob="**/*.pdf", # "*.pdf" opens any pdf file ; "**/" search this folder and subfolders (e.g. ./research_papers/paper1.pdf , ./research_papers/subfolder/paper2.pdf)
+        loader_cls=PyPDFLoader # choosing the type of loader
+        )
+    docs = loader.load()
+    
+    # data spliting
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    chunks = text_splitter.split_documents(docs)
+
+    # embed the chunks and store in Vector Database
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+    vectorstoredb = Chroma.from_documents(
+        chunks,
+        embedding_model,
+        persist_directory="./chroma_db", # save the vector store locally
+        collection_name="pdf_embedding" # specify a new name for this collection, bcs a persisted Chroma DB can hold more than one collection
+        )
+
+    # create the retriever
+    st.session_state.retriever = vectorstoredb.as_retriever() # store retriever in state memory
+  
+
+## wrapper function on create_rag_retriever() -> avoid rerunning the RAG chain
+def get_retriever(pdf_dir: Path):
+    if "retriever" not in st.session_state:
+        # create retriever
+        create_rag_retriever(pdf_dir)
+
+## create function to generate query reponse without RAG
+def generate_response_plain(question: str, system_prompt: str, model: str, temperature: float):
+    llm = ChatOpenAI(model=model, temperature=temperature)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}")
+        ]
+    )
+    chain = prompt | llm | StrOutputParser()
+    return chain.stream({"input": question})
+
+## create function to generate query response with RAG
+def format_docs(docs):
+    '''Join the page content of multiple Document objects'''
+    return "\n\n".join(d.page_content for d in docs)
+
+def generate_response_rag(retriever, question:str, system_prompt:str, model:str, temperature:float):
+    # create llm chain
+    llm = ChatOpenAI(model=model, temperature=temperature)
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", '''
+             <context>
+             {context}
+             </context>
+
+             <question>
+             {input}
+             </question>'''
+             )
+        ]
+    )
+    chain = (
+        {
+            "context": retriever | RunnableLambda(lambda x: format_docs(x)),
+            "input": RunnablePassthrough()
+        }
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    response = chain.stream(question)
+    return response
+
+## create a wrapper to choose whether to repond with or without RAG
+def generate_response(question: str, system_prompt: str, model: str, temperature: float, use_rag: bool):
+    if use_rag and "retriever" in st.session_state:
+        return generate_response_rag(st.session_state.retriever, question, system_prompt, model, temperature)
+
+    # either user chose no RAG, or retriever not ready
+    return generate_response_plain(question, system_prompt, model, temperature)
+
+
+
+
+
+
+
+
 # --- Sidebar to set model settings ---
 with st.sidebar:
     st.header("Model Settings")
@@ -29,8 +145,7 @@ with st.sidebar:
     # llm model selection
     model = st.selectbox(
         "Model",
-        # options=["gpt-4o-mini", "gpt-4o", "gpt-4.1-mini","gpt-4.1",],
-        options=["gpt-5-mini"],
+        options=["gpt-5-mini","gpt-4o-mini", "gpt-4o", "gpt-4.1-mini","gpt-4.1"],
         index=0,         # default selection
         help="Choose the OpenAI chat model to use"
     )
@@ -56,25 +171,72 @@ with st.sidebar:
         disabled= not show_system_prompt
         )
 
-# create function to generate query response
-def generate_response(question:str, system_prompt:str, model:str, temperature:float):
-    llm = ChatOpenAI(model=model, temperature=temperature)
-    prompt = ChatPromptTemplate(
-        [
-            ("system", system_prompt),
-            ("human", "{input}")
-        ]
-    )
-    chain = prompt | llm | StrOutputParser()
-    response = chain.invoke({"input": question})
-    return response
 
 # --- App UI ---
 ## Title of the app
 st.title("Q&A Chatbot With OpenAI")
 
+## file uploader: PDFs
+if "uploader_key" not in st.session_state:
+    st.session_state.uploader_key = 0
+
+uploaded_pdfs = st.file_uploader(
+    "Upload PDF files",
+    type=['pdf'],
+    accept_multiple_files=True,
+    key=f"key_pdf_uploader{st.session_state.uploader_key}" # this will be needed to reset the file_uploader
+    )
+
+col1, col2 = st.columns(2)
+with col1:
+    build = st.button("Build Retriever", type="primary", use_container_width=True)
+with col2:
+    clear = st.button("Clear Retriever", type="secondary", use_container_width=True)
+
+## Only build RAG when user clicks the button
+if build:
+    if not uploaded_pdfs:
+        st.error("Upload PDFs first before creating a retriever!")
+    else:
+        with st.spinner("Indexing PDFs (loasing -> Splitting -> embedding)..."):
+            try:
+                # create a temporary directory to store temporary files (in disk/permanent)
+                tmp_root = Path(tempfile.mkdtemp()) # create temporary folder with unique name, e.g. C:\Users\You\AppData\Local\Temp\tmpabc123
+                pdf_dir = tmp_root / "pdfs" # e.g. C:\Users\You\AppData\Local\Temp\tmpabc123\pdfs
+
+                # save the uploaded file in the temporary directory
+                save_uploads_to_dir(uploaded_pdfs, pdf_dir)
+
+                # Build the retriever
+                get_retriever(pdf_dir)
+                st.success("The Retriever is ready!")
+            
+            finally:
+                # remove the folder/files after uploading, as those files have been stored in the Vector Store
+                shutil.rmtree(tmp_root, ignore_errors=True) # "rmtree()" stands for "remove tree". This will delete the entire diretory tree (i.e. the folder and everything inside it)
+
+## Clear the retriever
+if clear:
+    st.session_state.pop("retriever", None)
+    st.session_state.uploader_key += 1 # change the key to reset the file_uploader
+    st.success("Cleared retriever!")
+    st.rerun()
+
 ## user to input question
 question = st.text_input("What can I help you today?")
+use_rag = st.checkbox("Use uploaded files (RAG)", value=True)
 
+## run the model
 if st.button("Submit") and question:
-    st.write(generate_response(question=question, model=model, temperature=temperature, system_prompt=system_prompt))
+    if use_rag and "retriever" not in st.session_state:
+        st.warning("RAG is enabled, but no retriever is built yet. Answering without uploaded files...")
+
+    with st.spinner("Generating response...", show_time=True):
+        st.write(generate_response(
+            question=question,
+            model=model,
+            temperature=temperature,
+            system_prompt=system_prompt,
+            use_rag=use_rag
+            )
+        )
